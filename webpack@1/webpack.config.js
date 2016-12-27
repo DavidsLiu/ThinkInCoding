68781bc6ca6827d65af6509d3fc51f4a82a72489
1162438119 <15751165579@163.com>
2016-12-27 18:01:40 +0800

    new
    new

--- webpack@1/webpack.config.js
+++ webpack@1/webpack.config.js
@@ -6,27 +6,27 @@ const webpack = require('webpack'),
 
 module.exports = {
   entry: {
-    index: './js/app.js'
+    index: './js/app.js' //入口
   },
   output: {
-    path: path.join(__dirname, 'dist/'),
-    filename: 'js/[name].[hash:8].js'
+    path: path.join(__dirname, 'dist/'), //输出的路径
+    filename: 'js/[name].[hash:8].js' //文件名称
   },
   module: {
     loaders: [
       {
         test: /\.less$/,
-        loader: ExtractTextPlugin.extract('style-loader','css-loader!less-loader!postcss-loader')
+        loader: ExtractTextPlugin.extract('style-loader','css-loader!less-loader!postcss-loader') //从js中分离出css
       }
     ]
   },
   postcss: function () {
-    return [Autoprefixer];
+    return [Autoprefixer]; //配置css前缀补全
   },
   plugins: [
-    new ExtractTextPlugin("css/[name].[hash:8].css"),
+    new ExtractTextPlugin("css/[name].[hash:8].css"),//提取出css的文件名称
     new HtmlPlugin({
-      template: './index.html'
+      template: './index.html' //html文件路径
     })
   ]
 }
