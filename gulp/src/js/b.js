0cfa1384a8b6c4e7d5829267109c3339411a12f9
1162438119 <15751165579@163.com>
2017-01-18 10:12:42 +0800

    new
    new

--- gulp/src/js/b.js
+++
@@ -1,8 +0,0 @@
-var list = document.querySelector('.list');
-
-list.addEventListener('click', function (e) {
-  var target = e.target;
-  if (target.tagName.toLowerCase() === 'li') {
-    target.style.background = "red";
-  }
-}, false);
--- gulp/src/js/index.js
+++
@@ -1,9 +0,0 @@
-function add(num1, num2) {
-  return num1 + num2;
-}
-
-var num1 = 20, num2 = 10;
-var result = add(num1, num2);
-
-var h = document.querySelector('h2');
-h.innerHTML = result + '';
